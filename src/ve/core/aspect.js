22f237fd8122c3e90fe23c428c41db6f0e293176
louisypchan <yia0223@gmail.com>
2014-10-30 08:04:45 +0800

    Add Aspect
    Add Aspect

--- src/ve/core/aspect.js
+++ src/ve/core/aspect.js
@@ -20,8 +20,108 @@
 /**
  * Created by Louis Y P Chen on 2014/10/23.
  */
+/**
+ * joinpoint — a point in the control flow.
+ *      Examples:
+ *          calling a method,
+ *          executing a method’s body or an exception handler,
+ *          referencing an object’s attribute, and so on.
+ *
+ * pointcut — a query that is used to define a set of affected joinpoints. Essentially this is a logical expression that can pick out joinpoints and make sure that their context is right.
+ *      Examples: it can verify that the affected object is of right type, that we are in particular control flow branch, and so on.
+ *
+ * advice — an additional behavior (a code) that will be applied at joinpoints.
+ *      Available advice types:
+ *          "before"            — runs before a joinpoint,
+ *          "after"             — runs after a joinpoint was executed,
+ *          "after returning"   — runs only after a normal execution of a joinpoint,
+ *          "after throwing"    — runs only if during execution of a joinpoint an unhandled exception was thrown.
+ *          "around"            — runs instead of a joinpoint, may call the original joinpoint.
+ *
+ * aspect — an entity that encapsulates related pointcuts, and advices together, and can add some attributes to advised classes.
+ */
 $.add(["public"], function(expose){
     "use strict";
 
+    function advise(inst, method){
+        this.next_before = this.prev_before =
+            this.next_after = this.prev_after = this.next_around = this.prev_around = this;
+        this.inst = inst;
+        this.method = method;
+    }
 
+    advise.prototype = {
+        add : function(before, after, around, target){
+            var advice = new advise(this.inst, this.method);
+            advice.advise = this;
+            advice.before = before;
+            advice.after = after;
+            advice.around = around;
+            advice.target = target;
+
+            this._add("before", advice);
+            this._add("around", advice);
+            this._add("after", advice);
+
+            if(around){
+                advice.target = (function(){ return advice.prev_around.target(around); })();
+            }
+
+            return advice;
+        },
+
+        _add : function(type, advice){
+            if(advice[type]){
+                var next = "next_" + type, prev = "prev_" + type;
+                //create chain
+                (advice[prev] = this[prev])[next] = (advice[next] = this)[prev] = advice;
+            }
+        },
+
+        remove : function(){
+
+        },
+
+        destory : function(){
+
+        }
+    };
+
+    function AOPmaker(advised){
+        var f =  function(){
+            var process;
+            //running before chain
+            for(process = advised.prev_before; process !== advised; process = process.prev_before){
+                process.before.apply(this, arguments);
+            }
+            //running the around chain
+
+            //running the after chain
+            for(process = advised.prev_after; process !== advised; process = process.prev_after){
+                process.after.apply(this, arguments);
+            }
+
+            return this;
+        };
+        f.advised = advised;
+        return f;
+    }
+
+    function weaver(inst, method, advice){
+        var f = inst[method], advised;
+        if(f && f.advised){
+            advised = f.advised;
+        }else{
+            advised = new advise(inst, method);
+            //construct the advice chians by target
+            advised.add(0, 0, 0, f);
+
+            inst[method] = AOPmaker(advised);
+        }
+        return advised.add(advice.before, advice.after, advice.around, 0);
+    }
+
+    expose.before = function(inst, method, advice) { return weaver(inst, method, {before : advice})};
+    expose.around = function(inst, method, advice) { return weaver(inst, method, {around : advice})};
+    expose.after = function(inst, method, advice) { return weaver(inst, method, {after : advice})};
 });
