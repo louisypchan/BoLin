6d98c354b968269f0f8619847b5b351f31826761
louisypchan <yia0223@gmail.com>
2014-11-05 19:11:52 +0800

    Add event object and apis
    Add event object and apis

--- src/ve/event/event.js
+++ src/ve/event/event.js
@@ -23,10 +23,91 @@
  * An extension to window.event
  *
  */
-$.add("ve/event/event", [], function(){
+$.add("ve/event/event", ["ve/core/kernel", "ve/extensions/date"], function(kernel){
     "use strict";
 
-    var lastEvt = {};
+    var hasStopimmediatepropagation = window.Event && !!window.Event.prototype && !!window.Event.prototype.stopImmediatePropagation,
+        lastEvent = {},
+        _event =  function(event){
+        var me = this, e, t;
+        if(typeof event == "object" && event.type){
+            me.originalEvent = e = event;
+            for(var name in e){
+                if(!kernel.isFunction){
+                    me[name] = e[name];
+                }
 
-    
+                if(e.extraData){
+                    kernel.extend(me, e.extraData);
+                }
+
+                me.target = me.srcElement = e.srcElement || (( t = e.target ) && ( t.nodeType == 3 ? t.parentNode : t ));
+
+                me.relatedTarget = e.relatedTarget || (( t = e.fromElement ) && ( t === me.target ? e.toElement : t ));
+                me.keyCode = me.which = e.keyCode || e.which;
+
+                //
+                if(me.type == "keypress"){
+                    var c = ("charCode" in e ? e.charCode : e.keyCode);
+                    if(c == 10){
+                        // CTRL-ENTER is CTRL-ASCII(10) on IE, but CTRL-ENTER on Mozilla
+                        c = 0;
+                        me.keyCode = 13;
+                    }else if(c == 13 || c == 27){
+                        // Mozilla considers ENTER and ESC non-printable
+                        c = 0;
+                    }else if(c == 3){
+                        // Mozilla maps CTRL-BREAK to CTRL-c
+                        c = 99;
+                    }
+                    //is it neccessary???
+                    me.charCode = c;
+                    me.keyChar = me.charCode ? String.fromCharCode(me.charCode) : '';
+                }
+                // Add which for mouseclick: 1 === left; 2 === middle; 3 === right
+                if(!me.which && e.button !== undefined){
+                    me.which = e.button & 1 ? 1 : ( e.button & 2 ? 3 : ( e.button & 4 ? 2 : 0 ) );
+                }
+
+                var doc = document.documentElement, body = document.body;
+
+                me.pageX = e.pageX || (e.clientX + (doc && doc.scrollLeft || body && body.scrollLeft || 0) - (doc && doc.clientLeft || body && body.clientLeft || 0));
+
+                me.pageY = e.pageY || (e.clientY + (doc && doc.scrollTop  || body && body.scrollTop  || 0) - (doc && doc.clientTop  || body && body.clientTop  || 0));
+
+                //TODO: add touch events
+            }
+        }
+
+        me.timeStamp = new Date().now();
+        me.__type__ = "@VE_EVENT";
+    };
+
+    kernel.extend(_event.prototype, {
+        stopPropagation : function(){
+            var e = this.originalEvent;
+            this.cancelBubble = e.cancelBubble = true;
+        },
+        preventDefault : function(){
+            var e = this.originalEvent;
+            this.bubbledKeyCode = this.keyCode;
+            if(e.ctrlKey){
+                try{
+                    this.keyCode = e.keyCode = 0;
+                }catch (e){}
+            }
+            this.defaultPrevented = e.defaultPrevented = true;
+            this.returnValue = e.returnValue = false;
+
+        },
+        stopImmediatePropagation : function () {
+            this.immediatelyStopped = true;
+        }
+    });
+
+    return function(event){
+        // should be same event, reuse event object (so it can be augmented);
+        if(event["__type__"] === "@VE_EVENT") return event;
+        return lastEvent && lastEvent.originalEvent === event ? lastEvent : new _event(event);
+    };
 });
