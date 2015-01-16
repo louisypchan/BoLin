8ccf074e5da745c50c1dc534b31f71d0c24fc898
Louis <yia0223@163.com>
2015-01-16 17:39:40 +0800

    deferred list

---
+++ src/bl/core/when.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 1/14/2015.
+ */
+$.add("bl/core/when", [], function(){
+   //deferred list
+    /**
+     * Take one(multiple) promise(s) and return a new promise
+     * when all promisees
+     */
+    var when = function(/*..*/){
+
+    }
+});
